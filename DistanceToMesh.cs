277e0396852cdcdc3e45b88f7a82a79ae873f038
xzbobzx <xzbobzx@live.nl>
2017-03-06 14:54:12 +0100

    added distanceToMesh.cs
    contains useful function things for checking distance to multiple things

---
+++ DistanceToMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceToMesh : MonoBehaviour {
+
+    public Transform cameraTransform;
+    public MeshFilter thisMesh;
+    public MeshRenderer thisRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Gameobject Distance: " + Vector3.Distance(thisRenderer.transform.position, cameraTransform.transform.position) + "\n" +
+            "Mesh Center Distance: " + Vector3.Distance(thisRenderer.bounds.center, cameraTransform.transform.position) + "\n" +
+            "Bound Closest Distance: " + Vector3.Distance(thisRenderer.bounds.ClosestPoint(cameraTransform.transform.position), cameraTransform.transform.position) + "");
+	}
+}
