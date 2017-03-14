651ba451c332dd0d24b9d648f5e75cfd017ffbbb
xzbobzx <xzbobzx@live.nl>
2017-03-14 11:22:35 +0100

    Added ResolveContiousPhysics
    this has nothing to do with Decimate, but I might as well use Decimate
    as my random junk github

---
+++ ResolveContinuousPhysics.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ResolveContinuousPhysics : MonoBehaviour {
+
+    Rigidbody rb;
+
+    //public Transform arrow;
+    //public bool debug;
+
+    private Vector3 onCollisionRotVel;
+    private Vector3 afterCollisionRotVel;
+
+    private Vector3 localV3;
+    private Vector3 globalV3;
+    private float force;
+    private Collision collision;
+    private float angle;
+
+    private Vector3 previousVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        previousVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter( Collision col )
+    {
+        onCollisionRotVel = rb.angularVelocity;
+
+        collision = col;
+
+        localV3 = transform.InverseTransformPoint(collision.contacts[0].point);
+        globalV3 = transform.TransformPoint(-localV3);
+
+        force = collision.impulse.magnitude / 2;
+
+        angle = Vector3.Angle(collision.contacts[0].normal, previousVelocity);
+        angle = angle - 90;
+        angle = angle / 90;
+
+        /*
+        if( debug )
+        {
+            Debug.Log(angle);
+
+            Instantiate(arrow, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
+            Instantiate(arrow, globalV3, Quaternion.LookRotation(-collision.contacts[0].normal));
+
+            Instantiate(arrow, rb.position, Quaternion.LookRotation(collision.contacts[0].normal));
+            Instantiate(arrow, rb.position, Quaternion.LookRotation(previousVelocity));
+        }
+        */
+
+        StartCoroutine("AfterCollision");
+    }
+
+    IEnumerator AfterCollision()
+    {
+        yield return new WaitForFixedUpdate();
+
+        afterCollisionRotVel = rb.angularVelocity;
+
+        if( (onCollisionRotVel - afterCollisionRotVel).magnitude < 1f )
+        {
+            float weight = 1;
+
+            if( collision.rigidbody != null )
+            {
+                if( rb.mass > collision.rigidbody.mass )
+                {
+                    weight = collision.rigidbody.mass / rb.mass;
+                }
+            }
+
+            rb.AddForceAtPosition(collision.contacts[0].normal * force * weight, collision.contacts[0].point, ForceMode.Impulse);
+            rb.AddForceAtPosition(-collision.contacts[0].normal * force * weight, globalV3, ForceMode.Impulse);
+
+            float friction;
+            Collider thisCollider = collision.contacts[0].thisCollider;
+
+            if( thisCollider.material.frictionCombine == PhysicMaterialCombine.Average )
+            {
+                friction = (thisCollider.material.dynamicFriction + collision.contacts[0].otherCollider.material.dynamicFriction) / 2;
+            }
+            else if( thisCollider.material.frictionCombine == PhysicMaterialCombine.Minimum )
+            {
+                friction = Mathf.Min(thisCollider.material.dynamicFriction, collision.contacts[0].otherCollider.material.dynamicFriction);
+            }
+            else if( thisCollider.material.frictionCombine == PhysicMaterialCombine.Maximum )
+            {
+                friction = Mathf.Max(thisCollider.material.dynamicFriction, collision.contacts[0].otherCollider.material.dynamicFriction);
+            }
+            else
+            {
+                friction = thisCollider.material.dynamicFriction * collision.contacts[0].otherCollider.material.dynamicFriction;
+            }
+
+            rb.AddForce(-rb.velocity * friction * angle, ForceMode.VelocityChange);
+
+            /*
+            if( debug )
+            {
+                Debug.Log("BAM");
+            }
+            */
+        }
+
+        /*
+        if( debug )
+        {
+            Debug.Log((onCollisionRotVel - afterCollisionRotVel) + " thus: " + afterCollisionRotVel.magnitude);
+        }
+        */
+    }
+}
